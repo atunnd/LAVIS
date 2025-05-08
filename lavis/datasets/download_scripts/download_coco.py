d23838a08ea885fb7001bca468447245038b0b28
atunnd <atuan.ngndai@gmail.com>
2025-05-08 17:27:23 +0700

    modify extract coco zip file

--- lavis/datasets/download_scripts/download_coco.py
+++ lavis/datasets/download_scripts/download_coco.py
@@ -7,6 +7,8 @@
 
 import os
 from pathlib import Path
+import zipfile
+
 
 from omegaconf import OmegaConf
 
@@ -19,16 +21,20 @@ from lavis.common.utils import (
 
 
 DATA_URL = {
-    "train": "http://images.cocodataset.org/zips/train2014.zip",  # md5: 0da8c0bd3d6becc4dcb32757491aca88
-    "val": "http://images.cocodataset.org/zips/val2014.zip",  # md5: a3d79f5ed8d289b7a7554ce06a5782b3
-    "test": "http://images.cocodataset.org/zips/test2014.zip",  # md5: 04127eef689ceac55e3a572c2c92f264
-    "test2015": "http://images.cocodataset.org/zips/test2015.zip",  # md5: 04127eef689ceac55e3a572c2c92f264
+    "train": "../train2014.zip",  # md5: 0da8c0bd3d6becc4dcb32757491aca88
+    "val": "../val2014.zip",  # md5: a3d79f5ed8d289b7a7554ce06a5782b3
+    "test": "../test2014.zip",  # md5: 04127eef689ceac55e3a572c2c92f264
+    "test2015": "../test2015.zip",  # md5: 04127eef689ceac55e3a572c2c92f264
 }
 
 
 def download_datasets(root, url):
     download_and_extract_archive(url=url, download_root=root, extract_root=storage_dir)
 
+def extract_local_zip(zip_path, extract_to):
+    print(f"Extracting {zip_path} to {extract_to}")
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
 
 if __name__ == "__main__":
 
@@ -45,13 +51,24 @@ if __name__ == "__main__":
         print(f"Dataset already exists at {storage_dir}. Aborting.")
         exit(0)
 
-    try:
-        for k, v in DATA_URL.items():
-            print("Downloading {} to {}".format(v, k))
-            download_datasets(download_dir, v)
-    except Exception as e:
-        # remove download dir if failed
-        cleanup_dir(download_dir)
-        print("Failed to download or extracting datasets. Aborting.")
+
+    print("ewewe")
+    print(DATA_URL.items())
+    for k, v in DATA_URL.items():
+        print("Downloading {} to {}".format(v, k))
+            #download_datasets(download_dir, v)
+        extract_local_zip(v, k)
+
+    # try:
+    #     print("ewewe")
+    #     print(DATA_URL.items())
+    #     for k, v in DATA_URL.items():
+    #         print("Downloading {} to {}".format(v, k))
+    #         #download_datasets(download_dir, v)
+    #         extract_local_zip(download_dir, storage_dir)
+    # except Exception as e:
+    #     # remove download dir if failed
+    #     cleanup_dir(download_dir)
+    #     print("Failed to download or extracting datasets. Aborting.")
 
     cleanup_dir(download_dir)
